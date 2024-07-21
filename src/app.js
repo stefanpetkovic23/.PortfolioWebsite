61d93a3e0fc59421b52bf18546f3b86572ae4aa8
stefanpetkovic23 <petkovicstefan36@gmail.com>
2024-07-21 14:05:40 +0200

    update again

--- src/app.js
+++ src/app.js
@@ -13,9 +13,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "../Front")));
-app.use("/css", express.static(__dirname, "../Assets"));
-app.use("/img", express.static(__dirname, "../CSS"));
-app.use("/js", express.static(__dirname, "../JS"));
+app.use("/css", express.static(__dirname, "../Front/Assets"));
+app.use("/img", express.static(__dirname, "../Front/CSS"));
+app.use("/js", express.static(__dirname, "../Front/JS"));
 
 // Postavi rutu za glavni HTML fajl
 app.get("/", (req, res) => {
